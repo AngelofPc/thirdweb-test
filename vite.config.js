c53fbabda8785fb369a8bc48d1587ed947a126f2
AceOluwapelumi <dagrandmaster01@gmail.com>
2023-05-15 15:17:38 +0100

    push

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import { nodePolyfills } from 'vite-plugin-node-polyfills';
 import * as path from 'path';
+import { cpus } from 'os';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -21,6 +22,22 @@ export default defineConfig({
       //   utils: 'utils',
       // process: 'process/browser',
       '@': path.resolve(__dirname, 'src'),
+    },
+
+    rollupOptions: {
+      maxParallelFileOps: Math.max(1, cpus().length - 1),
+      output: {
+        manualChunks: (id) => {
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        },
+        sourcemapIgnoreList: (relativeSourcePath) => {
+          const normalizedPath = path.normalize(relativeSourcePath);
+          return normalizedPath.includes('node_modules');
+        },
+      },
+      cache: false,
     },
   },
 });
