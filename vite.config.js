98d46d188a906b531a28a94ae22d46ed90ee60db
AceOluwapelumi <dagrandmaster01@gmail.com>
2023-05-22 16:59:20 +0100

    test

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import { nodePolyfills } from 'vite-plugin-node-polyfills';
+import * as path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
